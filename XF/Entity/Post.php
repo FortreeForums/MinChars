9f1c1c90a8165cd9a07268dbdbd5db086814437a
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-04-19 23:34:57 +0100

    Strip newlines to stop inflation

--- XF/Entity/Post.php
+++ XF/Entity/Post.php
@@ -24,10 +24,13 @@ class Post extends XFCP_Post
 			/* https://gist.github.com/madeinnordeste/e071857148084da94891 */
 			$message = preg_replace('/\b((https?|ftp|file):\/\/|www\.)[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/i', '', $message);
 			
+			/* Strip newlines to stop inflation */
+			$message = preg_replace('/(\s)*/', '', $message);
+			
 			/* Finally count the characters */
 			$chars = strlen($message);
 
-			// Only update if message is greater than X chars
+			/* Only update if message is greater than X chars */
 			if($chars > $options->ap_char_limit)
 			{
 				$this->User->fastUpdate('message_count', max(0, $this->User->message_count + $amount));
