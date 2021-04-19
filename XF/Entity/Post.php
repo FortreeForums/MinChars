8a3ae15156531f9f39443c0be2df062d18442e25
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-04-19 23:29:20 +0100

    Strip BBCode & URLs to stop inflation

--- XF/Entity/Post.php
+++ XF/Entity/Post.php
@@ -14,11 +14,20 @@ class Post extends XFCP_Post
 			&& $this->Thread->discussion_state == 'visible')
 		{
 			$options = \XF::options();
-
-			// Only update if message is greater than X chars
 			$message = $this->message;
+
+			/* Strip BBCode tags to stop inflation */		
+			$message = preg_replace('#\[[^]]*\]#', '', $message);
+			
+			/* Strip URLs to stop inflation */
+			/* Regex found on Github */
+			/* https://gist.github.com/madeinnordeste/e071857148084da94891 */
+			$message = preg_replace('/\b((https?|ftp|file):\/\/|www\.)[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/i', '', $message);
+			
+			/* Finally count the characters */
 			$chars = strlen($message);
 
+			// Only update if message is greater than X chars
 			if($chars > $options->ap_char_limit)
 			{
 				$this->User->fastUpdate('message_count', max(0, $this->User->message_count + $amount));
