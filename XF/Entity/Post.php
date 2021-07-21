4f07d2e0d4f62503e91aea250d0d2c3642ac733e
Fortree Forums <rennofthemill@hotmail.co.uk>
2021-07-21 16:18:54 +0100

    Exclude selected nodes from the check

--- XF/Entity/Post.php
+++ XF/Entity/Post.php
@@ -8,51 +8,59 @@ class Post extends XFCP_Post
 {
 	protected function adjustUserMessageCountIfNeeded($amount)
 	{
+		$options = \XF::options();
+		$forums = $options->ap_minchars_exclude_nodes;
+		
 		if ($this->user_id
 			&& $this->User
-			&& !empty($this->Thread->Forum->count_messages)
 			&& $this->Thread->discussion_state == 'visible')
 		{
-			$options = \XF::options();
 			$message = $this->message;
 			
-			if($options->ap_quote_check)
+			if(in_array($this->Thread->node_id, $forums))
 			{
-				/* Strip quoted text to stop inflation */
-				/* Regex found on StackOverflow */
-				/* https://stackoverflow.com/a/7208743 */
-				$message = preg_replace('/\[quote=(.*?)\](((?R)|.*?)+)\[\/quote\]/is', '', $message);
+				$chars = $options->ap_char_limit;
 			}
+			else
+			{
+				if($options->ap_quote_check)
+				{
+					/* Strip quoted text to stop inflation */
+					/* Regex found on StackOverflow */
+					/* https://stackoverflow.com/a/7208743 */
+					$message = preg_replace('/\[quote=(.*?)\](((?R)|.*?)+)\[\/quote\]/is', '', $message);
+				}
 
-			if($options->ap_bbcode_check)
-			{
-				/* Strip BBCode tags to stop inflation */	
-				$message = preg_replace('#\[[^]]*\]#', '', $message);
-			}
+				if($options->ap_bbcode_check)
+				{
+					/* Strip BBCode tags to stop inflation */	
+					$message = preg_replace('#\[[^]]*\]#', '', $message);
+				}
 			
-			if($options->ap_url_check)
-			{
-				/* Strip URLs to stop inflation */
-				/* Regex found on StackOverflow */
-				/* https://stackoverflow.com/a/54808354 */
-				$message = preg_replace('#((\w+:\/\/\S+)|(\w+[\.:]\w+\S+))[^\s,\.]#is', '', $message);
-			}
+				if($options->ap_url_check)
+				{
+					/* Strip URLs to stop inflation */
+					/* Regex found on StackOverflow */
+					/* https://stackoverflow.com/a/54808354 */
+					$message = preg_replace('#((\w+:\/\/\S+)|(\w+[\.:]\w+\S+))[^\s,\.]#is', '', $message);
+				}
 			
-			if($options->ap_newline_check)
-			{
-				/* Strip newlines to stop inflation */
-				$message = preg_replace('/(\s)*/', '', $message);
-			}
+				if($options->ap_newline_check)
+				{
+					/* Strip newlines to stop inflation */
+					$message = preg_replace('/(\s)*/', '', $message);
+				}
 						
-			/* Finally count the characters */
-			$chars = strlen($message);
-
-			/* Only update if message is greater than X chars */
-			if($chars > $options->ap_char_limit)
-			{
-				$this->User->fastUpdate('message_count', max(0, $this->User->message_count + $amount));
+				/* Finally count the characters */
+				$chars = strlen($message);
 			}
 		}
+		
+		/* Only update if message is greater than X chars */
+		if($chars >= $options->ap_char_limit)
+		{
+			$this->User->fastUpdate('message_count', max(0, $this->User->message_count + $amount));
+		}
+		
 	}
 }
-
